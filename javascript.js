717998b36d40feac28d53b334fd4340ae4e28224
Zach <107371302+FlipH19Switch@users.noreply.github.com>
2024-11-19 21:47:06 -0500

    Finish event listener for player info, add event listener for move info

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 const gameSquares = document.querySelectorAll('.game-square');
 const openDialog = document.querySelector('.open-dialog');
-const addPlayers = document.querySelector('.add-players')
+const addPlayers = document.querySelector('.add-players');
+const newMove = document.querySelectorAll('.game-square');
+const gameState = updateGame();
+let moveCount = 0;
 
 // listen for user to start game
 openDialog.addEventListener('click', () => {
@@ -8,13 +11,42 @@ openDialog.addEventListener('click', () => {
 });
 
 // listen for user to finish start form
-addPlayers.addEventListener('click', addPlayerInfo);
+addPlayers.addEventListener('click', storePlayerInfo);
 
-/* Current area of work
-function addPlayerInfo() {
+// listen for new moves on game board
+newMove.forEach(gameSquare => gameSquare.addEventListener('click', storeMoveInfo));
 
+function storeMoveInfo(e) {
+    const square = e.target.getAttribute('square-array-index');
+    const squareTextContent = e.target.textContent;
+    let symbol;
+
+    // check if square is already played
+    if (squareTextContent == "") {
+        moveCount++;
+    } else {
+        return
+    };
+
+    if (moveCount % 2 === 0) {
+        symbol = "O"
+    } else {
+        symbol = "X"
+    };
+
+    gameState.addSymbol(symbol, square);
+    gameState.updateSquare();
+    gameState.winnerCheck(symbol);
+};
+
+function storePlayerInfo() {
+
+    // grab player names from form
+    let playerOne = document.querySelector('#player-one');
+    let playerTwo = document.querySelector('#player-two');
+
+    return {playerOne, playerTwo};
 }
-*/
 
 function updateGame() {
     
@@ -27,52 +59,48 @@ function updateGame() {
         return gameBoard;
     };
 
-    // update tile display based on current state of gameboard
-    function updateTile () {
+    // update square display based on current state of gameboard
+    function updateSquare () {
         
         // loop through gameboard array
         for (let i = 0; i < gameBoard.length; i++) {
             
-            // get symbol for tile
-            let tileSymbol = gameBoard[i];
+            // get symbol for square
+            let squareSymbol = gameBoard[i];
 
-            // get div that matches tile position
-            let tileDisplay = document.querySelector(`div[square-array-index="${i}"]`)
+            // get div that matches square position
+            let squareDisplay = document.querySelector(`div[square-array-index="${i}"]`)
 
-            // update text content of tile
-            tileDisplay.textContent = tileSymbol;
+            // update text content of square
+            squareDisplay.textContent = squareSymbol;
         };
     };
 
     // check for winner after each move
     function winnerCheck (symbol) {
         if (
-            // check if tiles are not blank and tiles match vertically
+            // check if squares are not blank and square match vertically
             gameBoard[0] != "" && gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6] ||
             gameBoard[1] != "" && gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7] ||
             gameBoard[2] != "" && gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8] ||
             
-            // check if tiles are not blank and tiles match horizontally
+            // check if squares are not blank and square match horizontally
             gameBoard[0] != "" && gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2] ||
             gameBoard[3] != "" && gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5] ||
             gameBoard[6] != "" && gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8] ||
 
-            // check if tiles are not blank and tiles match diagonally
+            // check if squares are not blank and square match diagonally
             gameBoard[0] != "" && gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8] ||
             gameBoard[2] != "" && gameBoard[2] == gameBoard[4] && gameBoard[0] == gameBoard[6]
         ) {
-            alert(`${symbol} is the winner`)
+            // alert winner based on symbol
+            if (symbol == "X") {
+                alert `${storePlayerInfo.playerOne} is the winner`
+            } else alert `${storePlayerInfo.playerTwo} is the winner`
         } else if (!gameBoard.includes("")) {
             alert('Tie game')
         }
     };
 
-    return {addSymbol, updateTile, winnerCheck};
+    return {addSymbol, updateSquare, winnerCheck};
 };
-
-const gameState = updateGame();
-gameState.addSymbol("X",3);
-gameState.addSymbol("X",4);
-
-gameState.updateTile();
-gameState.winnerCheck("X");
